96b3680b4a6b69765617ee9455e2c010eb2d8b3e
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2024-10-16 22:11:01 -0300

    Trabalho 16/10/2024

---
+++ js/reveal.js
@@ -0,0 +1,111 @@
+/*
+========================================
+            ScrollReveal
+========================================
+*/
+window.revelar = ScrollReveal({reset:true})
+
+//Case Sucesso
+revelar.reveal('.card-animation1', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '120px',
+    delay: 200,
+    reset: false
+})
+revelar.reveal('.card-animation2', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '120px',
+    delay: 400,
+    reset: false
+})
+revelar.reveal('.card-animation3', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '120px',
+    delay: 600,
+    reset: false
+})
+revelar.reveal('.card-animation4', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '120px',
+    delay: 200,
+    reset: false
+})
+
+revelar.reveal('.card-animation5', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '120px',
+    delay: 400,
+    reset: false
+})
+
+revelar.reveal('.card-animation6', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '120px',
+    delay: 600,
+    reset: false
+})
+
+revelar.reveal('.card-animation7', {
+    duration: 1000,
+    origin: 'end',
+    distance: '120px',
+    delay: 850,
+    reset: false
+})
+
+revelar.reveal('.card-animation-top1', {
+    duration: 1000,
+    origin: 'top',
+    distance: '70px',
+    delay: 100,
+    reset: false
+})
+revelar.reveal('.card-animation-top2', {
+    duration: 1000,
+    origin: 'top',
+    distance: '70px',
+    delay: 200,
+    reset: false
+})
+revelar.reveal('.card-animation-top3', {
+    duration: 1000,
+    origin: 'top',
+    distance: '70px',
+    delay: 300,
+    reset: false
+})
+revelar.reveal('.card-animation-top4', {
+    duration: 1000,
+    origin: 'top',
+    distance: '70px',
+    delay: 400,
+    reset: false
+})
+revelar.reveal('.card-animation-top5', {
+    duration: 1000,
+    origin: 'top',
+    distance: '70px',
+    delay: 500,
+    reset: false
+})
+revelar.reveal('.card-animation-top6', {
+    duration: 1000,
+    origin: 'top',
+    distance: '70px',
+    delay: 600,
+    reset: false
+})
+revelar.reveal('.card-animation-top7', {
+    duration: 1000,
+    origin: 'top',
+    distance: '70px',
+    delay: 700,
+    reset: false
+})
+
---
+++ js/script.js
@@ -0,0 +1,47 @@
+const swiperOne = new Swiper('.swiper-case-sucesso-mobile', {
+    breakpoints: {
+             0: {
+                 slidesPerView: 1
+             },
+             992: {
+                 slidesPerView: 2
+             },
+             1024: {
+                 slidesPerView: 3
+             }
+    },
+    // navigation: {
+    //         nextEl: '.swiper-case-sucesso-mobile .swiper-button-next',
+    //         prevEl: '.swiper-case-sucesso-mobile .swiper-button-prev',
+    // },
+    spaceBetween: 30, // Espaço entre os slides
+    loop: true,
+    pagination: {
+      el: '.swiper-case-sucesso-mobile .swiper-pagination',
+      clickable: true,
+    },
+});
+
+const swiperTwo = new Swiper('.swiper-servicos-mobile', {
+    breakpoints: {
+             0: {
+                 slidesPerView: 1
+             },
+             992: {
+                 slidesPerView: 2
+             },
+             1024: {
+                 slidesPerView: 3
+             }
+    },
+    // navigation: {
+    //         nextEl: '.swiper-case-sucesso-mobile .swiper-button-next',
+    //         prevEl: '.swiper-case-sucesso-mobile .swiper-button-prev',
+    // },
+    spaceBetween: 30, // Espaço entre os slides
+    loop: true,
+    pagination: {
+      el: '.swiper-servicos-mobile .swiper-pagination',
+      clickable: true,
+    },
+});
