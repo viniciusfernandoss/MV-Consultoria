240efb456c6a8d62148e5aff11c9b92057c4584f
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2024-11-13 19:34:11 -0300

    Trabalho 13/11/2024

--- js/script.js
+++ js/script.js
@@ -45,3 +45,12 @@ const swiperTwo = new Swiper('.swiper-servicos-mobile', {
       clickable: true,
     },
 });
+
+window.onscroll = function() {
+    var containerH = document.getElementById('headerr');
+    if (window.scrollY > 1) { // Ajuste este valor conforme necessário
+        containerH.style.display = 'none';
+    } else {
+        containerH.style.display = 'block';
+    }
+};
